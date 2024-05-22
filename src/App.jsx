b09d06412aec6dc2aceb3af586016e34d0377408
vladislav-chihun <vladikkosmo@gmail.com>
2024-05-22 10:02:09 +0200

    а

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-
+import { apiFoo } from "./api";
 import SearchBar from "./components/SearchBar/SearchBar"
 import { useState } from "react";
 import ImageGallery from "./components/ImageGallery/ImageGallery"
 import Loader from "./components/Loader/Loader"
-
+import { useEffect } from "react";
 function App() {
   const [query, setQuery] = useState('');
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [images, setImages] = useState([]);
   function handleSearch(query) {
-
     setQuery(query)
 
+  }
+  useEffect(() => {
+    const fetchImages = async () => {
+      if (query) {
+        setIsLoading(true);
+        try {
+          const data = await apiFoo(query);
+          const imageData = data.results.map((image) => ({
+            smallImg: image.urls.small,
+            regularImg: image.urls.regular,
+          }));
+          setImages(imageData);
+        } catch (error) {
+          console.error(error);
+          setImages([]);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+    
+    fetchImages();
+  }, [query]);
+  if (images.length === 0) {
+    return null;
   }
   return (<>
     <SearchBar onSubmit={handleSearch} />
     {isLoading && <Loader />}
-    {!isLoading && <ImageGallery query={query} setIsLoading={setIsLoading}/>}
+    {!isLoading && <ImageGallery images={images} />}
   </>)
 }
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,40 +1,17 @@
-import { useState, useEffect } from "react";
-import { apiFoo } from "../../api";
 import ImageCard from "./ImageCard/ImageCard";
-import css from "./ImageGallery.module.css"
+import css from "./ImageGallery.module.css";
 
-export default function ImageGallery({ query,}) {
-  const [images, setImages] = useState([]);
-  
-  useEffect(() => {
-    const fetchImages = async () => {
-      if (query) {
-        try {
-          const data = await apiFoo(query); 
-          const imageData = data.results.map((image) => ({
-            smallImg: image.urls.small,
-            regularImg: image.urls.regular,
-          }));
-          setImages(imageData);
-        } catch (error) {
-          console.error(error);
-        } 
-      }
-    };
-
-    fetchImages();
-  }, [query]);
-
+export default function ImageGallery({ images }) {
   if (images.length === 0) {
     return null;
   }
 
-   return (
+  return (
     <div>
       <ul className={css.imgList}>
         {images.map((image, index) => (
           <li key={index}>
-             <ImageCard smallImg={image.smallImg} regularImg={image.regularImg} />
+            <ImageCard smallImg={image.smallImg} regularImg={image.regularImg} />
           </li>
         ))}
       </ul>
